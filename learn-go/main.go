df9d0ca94e5e0ca3b4bc18b839e2b4228ccb0551
youngkwon02 <youngkwon02@naver.com>
2021-11-14 02:25:22 +0900

    feat: Hello world and print, variable and const

---
+++ learn-go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World!")
+
+	const name string = "Youngkwon"
+	age := 25 // Only possible inside of a func
+	// var age int = 25
+
+	fmt.Println(name)
+	fmt.Println(age)
+}
